2a98377e0f3a60a771421f19bd76873d1bc4ae4a
Prajwol Amatya <prajwolamatya11@gmail.com>
2025-01-16 15:36:38 +0545

    implement mermaid js

--- js/vendor.js
+++ js/vendor.js
@@ -3,6 +3,7 @@ const vendorScripts = [
     'node_modules/reveal.js/plugin/notes/notes.js',
     'node_modules/revealjs-awesomd/plugin/awesoMD/awesoMD.js',
     'node_modules/reveal.js/plugin/highlight/highlight.js',
+    'node_modules/reveal.js-mermaid-plugin/plugin/mermaid/mermaid.js',
 ]
 
 module.exports = vendorScripts
